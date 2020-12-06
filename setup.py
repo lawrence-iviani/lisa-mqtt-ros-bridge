2ab57195ed61f4debbc26e2d4a908af09cd09cd7
Lawrence Iviani <lawrence.iviani@gmail.com>
2020-12-06 09:24:02 +0000

    Added beep in starting session

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
     packages=['lisa_mqtt'],
     package_dir={'': 'src'},
-    install_requires=['paho-mqtt', 'inject', 'msgpack-python', 'json', 'transitions']
+    install_requires=['paho-mqtt', 'inject', 'msgpack-python', 'json', 'transitions', 'soundfile']
 )
 
 setup(**d)
